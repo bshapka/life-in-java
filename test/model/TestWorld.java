f48bbb4705b16354b24cb3d83d5ffab0ee14a1fa
Brant Shapka <bshapka@gmail.com>
2021-06-25 15:37:30 -0700

    added test class for World class

--- test/model/TestWorld.java
+++ test/model/TestWorld.java
@@ -77,4 +77,163 @@ class TestWorld {
         assertEquals(expectedState, nextState);
     }
 
+    @Test
+    void testTetromino1StartState() {
+        startingState = Stream.of(
+                new Point(2, 3), new Point(3, 1), new Point(2, 1), new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        World world = new World(startingState);
+        world.nextState();
+        Set<Point> nextState = world.getState();
+        Set<Point> expectedState = Stream.of(
+                new Point(3, 1), new Point(1, 2), new Point(2, 1), new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        world.nextState();
+        nextState = world.getState();
+        expectedState = Stream.of(
+                new Point(1, 2), new Point(2, 1), new Point(3, 1), new Point(1, 1),
+                new Point(2, 2), new Point(3, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        expectedState = Stream.of(new Point(1, 2), new Point(3, 1), new Point(1, 1),
+                new Point(2, 0), new Point(2, 3), new Point(3, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        for (int i = 0; i < 10; i++) {
+            world.nextState();
+            nextState = world.getState();
+            assertEquals(expectedState, nextState);
+        }
+    }
+
+    @Test
+    void testTetromino2StartState() {
+        startingState = Stream.of(
+                new Point(3, 2), new Point(1, 2), new Point(4, 2), new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        World world = new World(startingState);
+        world.nextState();
+        Set<Point> nextState = world.getState();
+        Set<Point> expectedState = Stream.of(
+                new Point(2, 1), new Point(3, 1), new Point(2, 3), new Point(3, 3),
+                new Point(2, 2), new Point(3, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        expectedState = Stream.of(
+                new Point(1, 2), new Point(2, 1), new Point(3, 1), new Point(4, 2),
+                new Point(2, 3), new Point(3, 3))
+                .collect(Collectors.toCollection(HashSet::new));
+        for (int i = 0; i < 10; i++) {
+            world.nextState();
+            nextState = world.getState();
+            assertEquals(expectedState, nextState);
+        }
+    }
+
+    @Test
+    void testTetromino3StartState() {
+        startingState = Stream.of(
+                new Point(3, 2), new Point(3, 3), new Point(2, 1), new Point(3, 4))
+                .collect(Collectors.toCollection(HashSet::new));
+        World world = new World(startingState);
+        world.nextState();
+        Set<Point> nextState = world.getState();
+        Set<Point> expectedState = Stream.of(
+                new Point(4, 3), new Point(2, 3), new Point(3, 3), new Point(2, 2),
+                new Point(3, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        world.nextState();
+        nextState = world.getState();
+        expectedState = Stream.of(
+                new Point(3, 4), new Point(4, 3), new Point(4, 2), new Point(2, 3),
+                new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        world.nextState();
+        nextState = world.getState();
+        expectedState = Stream.of(
+                new Point(2, 3), new Point(3, 4), new Point(4, 3))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        world.nextState();
+        nextState = world.getState();
+        expectedState = Stream.of(
+                new Point(3, 3), new Point(3, 4))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        world.nextState();
+        nextState = world.getState();
+        expectedState = new HashSet<Point>();
+        assertEquals(expectedState, nextState);
+    }
+
+    @Test
+    void testTriomino1StartState() {
+        startingState = Stream.of(
+                new Point(3, 1), new Point(1, 2), new Point(2, 1))
+                .collect(Collectors.toCollection(HashSet::new));
+        World world = new World(startingState);
+        world.nextState();
+        Set<Point> nextState = world.getState();
+        Set<Point> expectedState = Stream.of(
+                new Point(2, 1), new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        world.nextState();
+        nextState = world.getState();
+        expectedState = new HashSet<Point>();
+        assertEquals(expectedState, nextState);
+    }
+
+    @Test
+    void testTriomino2StartState() {
+        startingState = Stream.of(
+                new Point(3, 2), new Point(1, 2), new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        World world = new World(startingState);
+        world.nextState();
+        Set<Point> nextState = world.getState();
+        Set<Point> expectedState = Stream.of(
+                new Point(2, 3), new Point(2, 1), new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        world.nextState();
+        nextState = world.getState();
+        assertEquals(startingState, nextState);
+    }
+
+    @Test
+    void testTriomino3StartState() {
+        startingState = Stream.of(
+                new Point(1, 1), new Point(1, 2), new Point(2, 1))
+                .collect(Collectors.toCollection(HashSet::new));
+        World world = new World(startingState);
+        Set<Point> expectedState = Stream.of(
+                new Point(1, 1), new Point(1, 2), new Point(2, 1), new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        for (int i = 0; i < 10; i++) {
+            world.nextState();
+            Set<Point> nextState = world.getState();
+            assertEquals(expectedState, nextState);
+        }
+    }
+
+    @Test
+    void testTriomino4StartState() {
+        startingState = Stream.of(
+                new Point(3, 1), new Point(1, 3), new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        World world = new World(startingState);
+        world.nextState();
+        Set<Point> nextState = world.getState();
+        Set<Point> expectedState = Stream.of(
+                new Point(2, 2))
+                .collect(Collectors.toCollection(HashSet::new));
+        assertEquals(expectedState, nextState);
+        world.nextState();
+        nextState = world.getState();
+        expectedState = new HashSet<Point>();
+        assertEquals(expectedState, nextState);
+    }
 }

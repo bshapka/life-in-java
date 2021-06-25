b537f1f5fbd39d4d545c9910be45b3f70ab66e96
Brant Shapka <bshapka@gmail.com>
2021-06-25 13:16:51 -0700

    added stub class World

---
+++ src/main/model/World.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class World {
+
+    // EFFECTS: instantiates a World by setting state
+    public World(Set<Point> startingState) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: updates state by applying the rules of the game
+    public void nextState() {
+        // stub
+    }
+
+    // EFFECTS: returns state
+    public HashSet<Point> getState() {
+        return new HashSet<Point>();
+    }
+
+}

8eadc1237f90988f97eeafc0baad7c9b7536c0db
Brant Shapka <bshapka@gmail.com>
2021-06-25 20:17:08 -0700

    implemented nextState and helper method region

--- src/model/World.java
+++ src/model/World.java
@@ -1,8 +1,12 @@
 package model;
 
 import java.awt.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 // represents a 2D world in grid form with each cell in the grid being live or dead (but not both)
 public class World {
@@ -17,12 +21,35 @@ public class World {
     // MODIFIES: this
     // EFFECTS: updates state by applying the rules of the game
     public void nextState() {
-        // stub
+        Set<Point> nextState = state.stream()
+                .map(World::region)
+                .flatMap(Collection::stream)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.reducing(0, e -> 1, Integer::sum)))
+                .entrySet().stream()
+                .filter(e -> e.getValue().equals(3) || (e.getValue().equals(4) && state.contains(e.getKey())))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toCollection(HashSet::new));
+        state = nextState;
+    }
+
+    // EFFECTS: returns the list of coordinates of the region around the given coordinate
+    //
+    // The region around a given coordinate is defined here to be the union of a) the Moore neighbourhood around the
+    // given coordinate and b) the given coordinate itself. In other words, the region around a coordinate is the
+    // coordinate itself plus the 8 closest other coordinates.
+    private static List<Point> region(Point point) {
+        Supplier<IntStream> streamSupplier = () -> IntStream.rangeClosed(-1, 1);
+        List<Point> region = new ArrayList<Point>();
+        streamSupplier.get().forEach(xOffset -> {
+            streamSupplier.get().forEach(yOffset -> {
+                region.add(new Point(point.x + xOffset, point.y + yOffset));
+            });
+        });
+        return region;
     }
 
     // EFFECTS: returns state
     public Set<Point> getState() {
         return state;
     }
-
 }

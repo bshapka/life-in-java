f484fc736f698e16fd60189e55f6cdafb42e239c
Brant Shapka <bshapka@gmail.com>
2021-06-26 00:24:57 -0700

    implemented getRandomState

--- src/ui/Game.java
+++ src/ui/Game.java
@@ -3,8 +3,9 @@ package ui;
 import model.World;
 
 import java.awt.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 // represents an instance of Conway's Game of Life
 public class Game {
@@ -19,7 +20,7 @@ public class Game {
         validateCellSize(cellSize);
         this.cellSize = cellSize;
         screenDimensions = getScreenDimensions();
-        Set<Point> randomState = getRandomState();
+        Set<Point> randomState = getRandomState(0.075f);
         world = new World(randomState);
     }
 
@@ -55,7 +56,21 @@ public class Game {
     }
 
     // EFFECTS: returns a random state scaled to fill the screen when rendered
-    private Set<Point> getRandomState() {
-        return new HashSet<Point>();
+    private Set<Point> getRandomState(float density) {
+        int screenArea = (int) screenDimensions.getWidth() * (int) screenDimensions.getHeight();
+        int cellArea = cellSize * cellSize;
+        int totalCells = screenArea / cellArea;
+        int desiredCells = (int) (totalCells * density);
+        int maxXCoordinate = (int) screenDimensions.getWidth() / cellSize;
+        int maxYCoordinate = (int) screenDimensions.getHeight() / cellSize;
+        List<Point> candidates = new ArrayList<Point>();
+        IntStream.rangeClosed(0, maxXCoordinate).forEach(i -> {
+            IntStream.rangeClosed(0, maxYCoordinate).forEach(j -> {
+                candidates.add(new Point(i, j));
+            });
+        });
+        Collections.shuffle(candidates);
+        HashSet<Point> randomState = new HashSet<Point>(candidates.subList(0, desiredCells));
+        return randomState;
     }
 }
